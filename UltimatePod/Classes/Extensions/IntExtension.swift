e20cb23cbc8a5677067bb0dbca1e61dfb688a32a
Bartek Żabicki <bartekzabicki@gmail.com>
2018-06-19 12:16:58 +0200

    Add multiple extensions - Localizable - Fades - Inspectable properties - Int as radians

---
+++ UltimatePod/Classes/Extensions/IntExtension.swift
@@ -0,0 +1,17 @@
+//
+//  IntExtension.swift
+//  CompanyManager
+//
+//  Created by Bartek on 09.04.2017.
+//  Copyright © 2017 zabicki. All rights reserved.
+//
+
+import UIKit
+
+extension Int {
+  
+  public func asRadian() -> CGFloat {
+    return (CGFloat(self) * CGFloat(Double.pi) / 180.0)
+  }
+  
+}
